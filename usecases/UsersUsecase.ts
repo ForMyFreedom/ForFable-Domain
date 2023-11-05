64379c9741edbad1c5a7b327cd1eeb3a58c214f3
Rafael <jiyuunotameni@hotmail.com>
2023-11-05 15:25:39 -0300

    front: wrong type in usecase

--- usecases/UsersUsecase.ts
+++ usecases/UsersUsecase.ts
@@ -1,12 +1,12 @@
 import { ApiResponse, EmailSended, GenericResponse, Pagination } from '.'
 import { ExceptionContract } from '../contracts'
-import { PromptEntity, ProposalEntity, RestartPasswordInsert, UserEntity, UserInsert } from '../entities'
+import { PromptEntity, ProposalEntity, RestartPasswordInsert, UserEntity, UserInsert, UserUpdate } from '../entities'
 
 export interface UsersUsecase {
   index(page?: number, limit?: number): Promise<Pagination<UserEntity>>
   show(userId: UserEntity['id']): Promise<ApiResponse<UserEntity>>
   store(body: UserInsert, isAdmin: boolean): Promise<ApiResponse<UserEntity>>
-  update(responserId: UserEntity['id']|undefined, userId: UserEntity['id'], partialBody: Partial<UserInsert>): Promise<ApiResponse<UserEntity>>
+  update(responserId: UserEntity['id']|undefined, userId: UserEntity['id'], partialBody: Partial<UserUpdate>): Promise<ApiResponse<UserEntity>>
   destroy(responserId: UserEntity['id']|undefined, userId: UserEntity['id']): Promise<ApiResponse<UserEntity>>
   verifyEmail(token: string|undefined): Promise<ApiResponse<boolean>>
   requestPasswordChange(user: UserEntity|undefined): Promise<ApiResponse<EmailSended>>
@@ -17,7 +17,7 @@ export interface UsersUsecase {
 export interface UsersController extends Omit<UsersUsecase, 'store'|'update'|'destroy'|'restartPassword'> {
   storeAdmin(body: UserInsert): Promise<ApiResponse<UserEntity>>
   storeUser(body: UserInsert): Promise<ApiResponse<UserEntity>>
-  update(userId: UserEntity['id'], partialBody: Partial<UserInsert>): Promise<ApiResponse<UserEntity>>
+  update(userId: UserEntity['id'], partialBody: Partial<UserUpdate>): Promise<ApiResponse<UserEntity>>
   destroy(userId: UserEntity['id']): Promise<ApiResponse<UserEntity>>
   restartPassword(token: string|undefined, body: RestartPasswordInsert): Promise<GenericResponse>
 }
